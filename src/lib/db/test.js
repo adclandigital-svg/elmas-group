0a90882bde2e6df99e91608445a39add9244ce7b
adclandigital-svg <adclandigital@gmail.com>
2026-05-20 18:38:52 +0530

    abc

--- src/lib/db/test.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const password = '123456';
-const salt = bcrypt.genSaltSync(10);
-const hashedPassword = bcrypt.hashSync(password, salt);
-
-console.log('Password:', password);
-console.log('Hash:', hashedPassword);
