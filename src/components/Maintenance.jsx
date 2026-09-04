813605868c9550c557113611ca052ad6c0a2ec4d
iotechsoftwares-web <iotechsoftwares@gmail.com>
2026-09-04 13:04:51 +0530

    Correction on maintenance page

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -6,7 +6,7 @@ import "./maintenance.css";
 export default function Maintenance() {
   return (
     <div className="maintenance-wrapper">
-      <div className="maintenance-overlay"></div>
+      {/* <div className="maintenance-overlay"></div> */}
       <div className="maintenance-content">
         <img src="/assets/logo.png" alt="Elmas Group Logo" className="maintenance-logo" />
         <h1 className="maintenance-text">Something amazing is coming!</h1>
